7492b54d9aa882637d734d3f064663033933b961
Folafolu <ofolafolu@gmail.com>
2025-01-24 19:37:58 +0100

    staticfiles

--- payment_gateway/views.py
+++ payment_gateway/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to payment gateway")
+    return HttpResponse("Welcome to payment gateway. Go to api/v1/docs ")
